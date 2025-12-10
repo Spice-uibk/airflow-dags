b88a3d7d58238bfe1e643d70ed4630894ca3a4ac
Stefan Pedratscher <Stefan.Pedratscher@uibk.ac.at>
2025-12-10 12:48:33 +0100

    added dynamic parallelism example

---
+++ dags/hello_world_k8s_dynamic.py
@@ -0,0 +1,46 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.decorators import task
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from airflow.operators.empty import EmptyOperator
+
+default_args = {
+    'owner': 'stefanpedratscher',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
+dag = DAG(
+    'hello_world_kubernetes_dynamic',
+    default_args=default_args,
+    description='A simple Hello World DAG using Kubernetes operator',
+    schedule='@once',
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    tags=['hello_world', 'k8s'],
+)
+
+with dag:
+    start_task = EmptyOperator(task_id='start')
+
+    @task
+    def get_input_list():
+        return ["Instance 1", "Instance 2", "Instance 3"]
+
+    input_data = get_input_list()
+
+    k8s_hello_task = KubernetesPodOperator.partial(
+        task_id='k8s_hello',
+        name='hello-pod',
+        namespace='stefan-dev',
+        image='python:3.9-slim',
+        cmds=['python', '-c'],
+        in_cluster=True,
+    ).expand(
+        arguments=input_data.map(lambda x: [f'print("Hello from {x}!")'])
+    )
+    end_task = EmptyOperator(task_id='end')
+    start_task >> input_data >> k8s_hello_task >> end_task
