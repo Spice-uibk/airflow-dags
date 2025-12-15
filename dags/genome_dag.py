1aa48d9eb2bf0641404796b5b688f256536107eb
Elias Kogler <elias.kogler@student.uibk.ac.at>
2025-12-15 10:11:26 +0100

    cahnged pull policy

--- dags/genome_dag.py
+++ dags/genome_dag.py
@@ -67,7 +67,7 @@ with DAG(
            env_vars=minio_env_dict,
            get_logs=True,
            is_delete_operator_pod=False,
-           image_pull_policy="Always",
+           image_pull_policy="IfNotPresent",
            execution_timeout=timedelta(hours=1),
            node_selector={"kubernetes.io/hostname": "node1"},
         )
